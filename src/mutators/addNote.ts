4ad4b3da6dac67806fa7e731cd67c8d0d6037005
Marcos Rivas <marcosrivasr@gmail.com>
2021-12-31 15:09:02 -0600

    mutators added

--- src/mutators/addNote.ts
+++ src/mutators/addNote.ts
@@ -1,18 +1,44 @@
 import { mutator } from "satcheljs";
-import { addNote, selectNode } from "../actions/notes";
+import {
+  addNote,
+  selectNode,
+  updateContent,
+  updateTitle,
+} from "../actions/notes";
 import getStore from "../store/store";
+import { v4 as uuidv4, v4 } from "uuid";
 
 mutator(addNote, (actionMessage) => {
   getStore().noteItems.unshift({
-    id: Math.random().toString(),
+    id: v4(),
     title: actionMessage.title,
-    content: actionMessage.content,
+    content: undefined,
     createdAt: Date.now(),
   });
 });
 
 mutator(selectNode, (actionMessage) => {
-  const note = getStore().noteItems[actionMessage.index];
+  let note;
+  if (typeof actionMessage.id === "number") {
+    note = getStore().noteItems[actionMessage.id];
+  } else {
+    note = getStore().noteItems.find((item) => item.id === actionMessage.id);
+  }
+  if (note) {
+    getStore().selected = note;
+  }
+});
 
-  getStore().selected = { ...note };
+mutator(updateContent, (actionMessage) => {
+  const selectedNote = getStore().selected!;
+  const id = selectedNote.id;
+
+  let item = getStore().noteItems.find((item) => item.id === id);
+  item!.content = { ...actionMessage.content };
+
+  getStore().selected = item!;
+});
+
+mutator(updateTitle, (actionMessage) => {
+  getStore().selected!.title = actionMessage.title;
 });
